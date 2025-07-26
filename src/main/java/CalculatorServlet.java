451ea6d7760c0b51fdc78db77c615ffd2f12da9a
Jeevankumar37 <jeevankumar.karupothula@gmail.com>
2025-07-26 21:38:35 +0530

    updated code

--- src/main/java/CalculatorServlet.java
+++ src/main/java/CalculatorServlet.java
@@ -1,26 +1,40 @@
 package com.example.calculator;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.*;
 
 @WebServlet("/calculate")
 public class CalculatorServlet extends HttpServlet {
 
+    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-        throws ServletException, IOException {
-        
+            throws ServletException, IOException {
+
         double num1 = Double.parseDouble(request.getParameter("num1"));
         double num2 = Double.parseDouble(request.getParameter("num2"));
         String operator = request.getParameter("operator");
 
         double result = 0;
         switch (operator) {
-            case "+": result = num1 + num2; break;
-            case "-": result = num1 - num2; break;
-            case "*": result = num1 * num2; break;
-            case "/": result = num2 != 0 ? num1 / num2 : 0; break;
+            case "+":
+                result = num1 + num2;
+                break;
+            case "-":
+                result = num1 - num2;
+                break;
+            case "*":
+                result = num1 * num2;
+                break;
+            case "/":
+                result = (num2 != 0) ? num1 / num2 : 0;
+                break;
+            default:
+                result = 0;
         }
 
         request.setAttribute("result", result);
